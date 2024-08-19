394fe3fc33bb123f37fa357bb69149d8c69fddce
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-08-20 04:00:05 +0600

    vue started

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+/* Add some basic styling */
+nav {
+  margin-bottom: 20px;
+}
+
+nav a {
+  margin-right: 10px;
+  text-decoration: none;
+  color: #42b983;
+}
+
+nav a.router-link-active {
+  font-weight: bold;
+}
+</style>
