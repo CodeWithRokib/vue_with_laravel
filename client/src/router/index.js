22811cad8b13e20e2bc6ec1178bc1883ced5405d
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-08-24 23:42:21 +0600

    login register route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,29 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
-import HelloWorld from '../components/HelloWorld.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import HelloWorld from "../components/HelloWorld.vue";
+import Register from "../views/Register.vue";
+import Login from "../views/Login.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: "/about",
+    name: "About",
+    component: About,
   },
   {
-    path: '/home',
-    name: 'HelloWorld',
-    component: HelloWorld
-  }
+    path: "/home",
+    name: "HelloWorld",
+    component: HelloWorld,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
 
 export default router;
