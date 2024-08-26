318482c631393b9341ddeea009f55742eeb5650e
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-08-26 23:23:09 +0600

    contact showed

--- client/src/views/ContactForm.vue
+++ client/src/views/ContactForm.vue
@@ -1,23 +1,36 @@
 <template>
-    <form @submit.prevent="submitForm">
-        <div>
-            <label for="name">Name:</label>
-            <input type="text" v-model="contact.name" required />
-        </div>
-        <div>
-            <label for="email">Email:</label>
-            <input type="email" v-model="contact.email" required />
-        </div>
-        <div>
-            <label for="phone">Phone:</label>
-            <input type="text" v-model="contact.phone" required />
-        </div>
-        <div>
-            <label for="description">Description:</label>
-            <textarea v-model="contact.description" required></textarea>
-        </div>
-        <button type="submit">Submit</button>
-    </form>
+    <div>
+        <h1>Contact Us</h1>
+        <form @submit.prevent="submitForm">
+            <div>
+                <label for="name">Name:</label>
+                <input type="text" v-model="contact.name" required />
+            </div>
+            <div>
+                <label for="email">Email:</label>
+                <input type="email" v-model="contact.email" required />
+            </div>
+            <div>
+                <label for="phone">Phone:</label>
+                <input type="text" v-model="contact.phone" required />
+            </div>
+            <div>
+                <label for="description">Description:</label>
+                <textarea v-model="contact.description" required></textarea>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+
+        <hr />
+
+        <h2>All Contacts</h2>
+        <ul>
+            <li v-for="contact in contacts" :key="contact.id">
+                <strong>{{ contact.name }}</strong> - {{ contact.email }} - {{ contact.phone }}<br />
+                <em>{{ contact.description }}</em>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -31,7 +44,8 @@ export default {
                 email: '',
                 phone: '',
                 description: ''
-            }
+            },
+            contacts: [] // To store all the contacts
         };
     },
     methods: {
@@ -40,8 +54,18 @@ export default {
                 const response = await axios.post('http://localhost:8000/api/contacts', this.contact);
                 alert(response.data.message);
                 this.resetForm();
+                this.fetchContacts(); // Refresh the contacts list after submitting the form
             } catch (error) {
-                console.error('There was an error submitting the form!', error);
+                console.error('There was an error submitting the form!', error.response.data);
+                alert('Error: ' + (error.response.data.message || 'Unknown error'));
+            }
+        },
+        async fetchContacts() {
+            try {
+                const response = await axios.get('http://localhost:8000/api/contacts');
+                this.contacts = response.data;
+            } catch (error) {
+                console.error('There was an error fetching the contacts!', error);
             }
         },
         resetForm() {
@@ -52,6 +76,9 @@ export default {
                 description: ''
             };
         }
+    },
+    mounted() {
+        this.fetchContacts(); // Fetch the contacts when the component is mounted
     }
 };
 </script>
